67e7c5e5c57898235a4508a49bd0fad20fb237ba
charles <charles@intersect.org.au>
2013-06-11 12:20:59 +1000

    Track progress of zip and allow optional async packaging

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -22,12 +22,24 @@ class PackagesController < DataFilesController
     if @package.save
       data_file_ids = current_user.cart_item_ids
       begin
-        # Resque.enqueue(PackageWorker, @package.id, data_file_ids, current_user.id)
-        # Persist the job id in the db - we need to retrieve it per record basis
-        @package.uuid = PackageWorker.create({:package_id => @package.id, :data_file_ids => data_file_ids, :user_id => current_user.id})
-        @package.save
 
-        redirect_to data_file_path(@package), notice: 'Package was successfully created.'
+        if params[:run_in_background]
+          # Persist the job id in the db - we need to retrieve it per record basis
+          @package.uuid = PackageWorker.create({:package_id => @package.id, :data_file_ids => data_file_ids, :user_id => current_user.id})
+          job = Resque::Plugins::Status::Hash.get(@package.uuid)
+          @package.transfer_status = job.status.to_s.upcase
+          @package.save
+          redirect_to data_file_path(@package), notice: 'Package is now queued for processing in the background.'
+        else
+          # Run normally
+          CustomDownloadBuilder.bagit_for_files_with_ids(data_file_ids, @package) do |zip_file|
+            attachment_builder = AttachmentBuilder.new(APP_CONFIG['files_root'], current_user, FileTypeDeterminer.new, MetadataExtractor.new)
+            package = attachment_builder.build_package(@package, zip_file)
+            build_rif_cs(package) unless package.nil?
+          end
+          redirect_to data_file_path(@package), notice: 'Package was successfully created.'
+        end
+
       rescue ::TemplateError => e
         logger.error e.message
         redirect_to data_file_path(@package), alert: 'There were errors in the README.html template file.'
@@ -58,6 +70,22 @@ class PackagesController < DataFilesController
     end
 
     redirect_to data_files_path, :notice => valid ? "Package has been successfully submitted for publishing." : "Unable to publish package."
+  end
+
+  def build_rif_cs(files)
+    #build the rif-cs and place in the unpublished_rif_cs folder, where it will stay until published in DC21
+    dir = APP_CONFIG['unpublished_rif_cs_directory']
+    Dir.mkdir(dir) unless Dir.exists?(dir)
+    output_location = File.join(dir, "rif-cs-#{@package.id}.xml")
+
+    file = File.new(output_location, 'w')
+
+    options = {:root_url => root_url,
+               :collection_url => data_file_path(@package),
+               :zip_url => download_data_file_url(@package),
+               :submitter => current_user}
+    RifCsGenerator.new(PackageRifCsWrapper.new(@package, files, options), file).build_rif_cs
+    file.close
   end
 
   def publish_rif_cs
--- app/workers/package_worker.rb
+++ app/workers/package_worker.rb
@@ -1,10 +1,9 @@
+require 'bagit'
 class PackageWorker
   include Resque::Plugins::Status
 
   @queue = :package_queue
 
-  PACKAGE_QUEUED = 'QUEUED'
-  PACKAGE_WORKING = 'PACKAGING'
   PACKAGE_COMPLETE = 'COMPLETE'
 
   def perform
@@ -15,17 +14,24 @@ class PackageWorker
     user = User.find(user_id)
     pkg = DataFile.find(package_id)
 
-    pkg.transfer_status = PACKAGE_QUEUED
+    job = Resque::Plugins::Status::Hash.get(pkg.uuid)
+    pkg.transfer_status = job.status.to_s.upcase
     pkg.save!
 
-    CustomDownloadBuilder.bagit_for_files_with_ids(data_file_ids, pkg) do |zip_file|
-      attachment_builder = AttachmentBuilder.new(APP_CONFIG['files_root'], user, FileTypeDeterminer.new, MetadataExtractor.new)
-      files = attachment_builder.build_package(pkg, zip_file)
-      build_rif_cs(files, pkg, user) unless files.nil?
+    begin
+      bagit_for_files_with_ids(data_file_ids, pkg) do |zip_file|
+        attachment_builder = AttachmentBuilder.new(APP_CONFIG['files_root'], user, FileTypeDeterminer.new, MetadataExtractor.new)
+        files = attachment_builder.build_package(pkg, zip_file)
+        build_rif_cs(files, pkg, user) unless files.nil?
+      end
+
+      # Since the parent of the action can't update it
+      pkg.transfer_status = PACKAGE_COMPLETE
+      pkg.save!
+    rescue
+      pkg.transfer_status = job.status.to_s.upcase
+      pkg.save!
     end
-
-    pkg.transfer_status = PACKAGE_COMPLETE
-    pkg.save!
   end
 
   private
@@ -44,6 +50,59 @@ class PackageWorker
                :submitter => user}
     RifCsGenerator.new(PackageRifCsWrapper.new(package, files, options), file).build_rif_cs
     file.close
+  end
+
+
+  def bagit_for_files_with_ids(ids, pkg, &block)
+    temp_dir = Dir.mktmpdir
+    zip_file = Tempfile.new("download_zip")
+
+    begin
+      bag = BagIt::Bag.new temp_dir
+      readme_path = File.join(bag.data_dir, "README.html")
+
+      data_files = DataFile.find(ids)
+      data_files.each do |data_file|
+        temp_path = File.join(bag.data_dir, data_file.filename)
+        FileUtils.cp data_file.path, temp_path
+        temp_path
+      end
+
+      readme_html = MetadataWriter.generate_metadata_for(data_files, pkg)
+      File.open(readme_path, 'w+') { |f| f.write(readme_html) }
+
+      bag.manifest!
+
+      number_of_files = data_files.length
+      build_zip(zip_file, Dir["#{temp_dir}/*"], number_of_files)
+      block.yield(zip_file)
+    ensure
+      zip_file.close
+      zip_file.unlink
+      FileUtils.remove_entry_secure temp_dir
+    end
+  end
+
+  def build_zip(zip_file, file_paths, number_of_files)
+    Zip::ZipOutputStream.open(zip_file.path) do |zos|
+      data_files_processed = 0
+      file_paths.each do |path|
+        if File.directory?(path)
+          dir_name = File.basename(path)
+          all_files = Dir.foreach(path).reject { |f| f.starts_with?(".") }
+          all_files.each do |file|
+            zos.put_next_entry("#{dir_name}/#{file}")
+            zos << File.open(File.join(path,file), 'rb') { |file| file.read }
+          end
+          data_files_processed += 1
+        else
+          zos.put_next_entry(File.basename(path))
+          zos << File.open(path, 'rb') { |file| file.read }
+          data_files_processed += 1
+        end
+        at(data_files_processed, number_of_files, "Processed #{data_files_processed} of #{number_of_files} data files...")
+      end
+    end
   end
 
 end
